61c1edd59579daba8c381a3ce367d7204eb8b4df
Pablo <pabloduz@gmail.com>
2016-11-01 10:08:37 -0200

    Adicionando todos os arquivos.

--- bd/zapmoveis.sql
+++ bd/zapmoveis.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 23-Out-2016 às 15:41
+-- Generation Time: 01-Nov-2016 às 13:06
 -- Versão do servidor: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -30,6 +30,7 @@ CREATE TABLE `imoveis` (
   `id` int(11) NOT NULL,
   `modelo` int(11) NOT NULL,
   `valor` int(11) NOT NULL,
+  `estilo` int(11) NOT NULL,
   `quarto` int(11) NOT NULL,
   `patio` int(11) NOT NULL,
   `piscina` int(11) NOT NULL,
@@ -41,19 +42,27 @@ CREATE TABLE `imoveis` (
 -- Extraindo dados da tabela `imoveis`
 --
 
-INSERT INTO `imoveis` (`id`, `modelo`, `valor`, `quarto`, `patio`, `piscina`, `garagem`, `academia`) VALUES
-(1, 1, 1, 0, 0, 1, 1, 0),
-(2, 1, 1, 0, 0, 1, 2, 0),
-(3, 1, 1, 0, 0, 2, 1, 0),
-(4, 1, 1, 0, 0, 2, 2, 0),
-(5, 1, 2, 0, 0, 1, 1, 0),
-(6, 1, 2, 0, 0, 2, 2, 0),
-(7, 1, 2, 0, 0, 1, 2, 0),
-(8, 1, 2, 0, 0, 2, 1, 0),
-(9, 2, 1, 0, 0, 0, 0, 1),
-(10, 2, 1, 0, 0, 0, 0, 2),
-(11, 2, 2, 0, 0, 0, 0, 1),
-(12, 2, 2, 0, 0, 0, 0, 2);
+INSERT INTO `imoveis` (`id`, `modelo`, `valor`, `estilo`, `quarto`, `patio`, `piscina`, `garagem`, `academia`) VALUES
+(1, 1, 1, 1, 0, 0, 1, 1, 0),
+(2, 1, 1, 1, 0, 0, 1, 2, 0),
+(3, 1, 1, 1, 0, 0, 2, 1, 0),
+(4, 1, 1, 1, 0, 0, 2, 2, 0),
+(5, 1, 2, 1, 0, 0, 1, 1, 0),
+(6, 1, 2, 1, 0, 0, 2, 2, 0),
+(7, 1, 2, 1, 0, 0, 1, 2, 0),
+(8, 1, 2, 1, 0, 0, 2, 1, 0),
+(9, 1, 1, 2, 0, 0, 1, 1, 0),
+(10, 1, 1, 2, 0, 0, 1, 2, 0),
+(11, 1, 1, 2, 0, 0, 2, 1, 0),
+(12, 1, 1, 2, 0, 0, 2, 2, 0),
+(13, 1, 2, 2, 0, 0, 1, 1, 0),
+(14, 1, 2, 2, 0, 0, 2, 2, 0),
+(15, 1, 2, 2, 0, 0, 1, 2, 0),
+(16, 1, 2, 2, 0, 0, 2, 1, 0),
+(17, 2, 1, 0, 0, 0, 0, 0, 1),
+(18, 2, 1, 0, 0, 0, 0, 0, 2),
+(19, 2, 2, 0, 0, 0, 0, 0, 1),
+(20, 2, 2, 0, 0, 0, 0, 0, 2);
 
 --
 -- Indexes for dumped tables
@@ -73,7 +82,7 @@ ALTER TABLE `imoveis`
 -- AUTO_INCREMENT for table `imoveis`
 --
 ALTER TABLE `imoveis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
